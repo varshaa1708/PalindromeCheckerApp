b59080c288b940cd929dc524cc6d88a685605b43
varshaa1708 <gvarshaa2006@gmail.com>
2026-02-12 11:15:31 +0530

    Fix formatting of version output in PalindromeCheckerApp

--- palindromechecker/src/PalindromeCheckerApp.java
+++ palindromechecker/src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("welcome to Palindrome Checker Management System");
-          System.out.println("version :1.0");
+          System.out.println("version: 1.0");
         System.out.println("System Initiated Successfully");
         
     }
